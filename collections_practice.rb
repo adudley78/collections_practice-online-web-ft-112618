11ef85e47075159763c721bed91c2e299cf6174f
Adam Dudley <dudleyadam@gmail.com>
2018-11-28 23:54:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
+def sort_array_asc(items)
+  items.sort
+end
 
+def sort_array_desc(items)
+  items.sort.reverse
+end
+
+def sort_array_char_count(items)
+  items.sort_by { |item| item.length }
+end
+
+def swap_elements(items)
+  items.sort { |a,b| a <=> b }
+end
+
+def reverse_array(items)
+  items.sort_by do |item|
+    -item
+  #{ |x,y| -(x <=> y) }
+  end
+end
